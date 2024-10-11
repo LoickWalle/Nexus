b2917255398df0842315ae23327623d405df9ec8
Loick Walle <loickwalle@gmail.com>
2024-10-11 14:12:51 +0200

    Ajout back de base

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/SpaceBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpaceBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceBackApplication.class, args);
+	}
+
+}
---
+++ SpaceBack/src/test/java/com/example/SpaceBack/SpaceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpaceBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
