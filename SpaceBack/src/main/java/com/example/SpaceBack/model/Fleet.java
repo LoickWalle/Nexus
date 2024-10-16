975b8eb0238005b11f765d24e6ea16d6bb95a96a
Julien Brancourt <115453167+JulienBrancourt@users.noreply.github.com>
2024-10-16 16:26:46 +0200

    Merge Feature/0003 (#8)
    * 0003/feat: Add Fleet entity
    
    * 0003/feat: Add Fleet entity with builder
    
    * 0003/fix: Remove type HashSet

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/model/Fleet.java
@@ -0,0 +1,40 @@
+package com.example.SpaceBack.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fleet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    private int destinationX;
+
+    private int destinationY;
+
+    @DateTimeFormat
+    private LocalDate arrivalTime;
+
+//    @OneToMany(mappedBy = "fleet")
+//    @Builder.Default
+//    private Set<FleetShip> fleetShips = new HashSet<>();
+
+
+}
