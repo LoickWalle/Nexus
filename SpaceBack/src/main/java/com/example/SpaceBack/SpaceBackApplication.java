0060b4dc3705d9e8b1d483cbb3d87868e780e245
LoickWalle <loickwalle@gmail.com>
2024-10-17 14:18:24 +0200

    Revert "Feature/0005 (#3)"
    This reverts commit a2078b6b5560960a956e4f7791f61364a4ffd6b1.

--- SpaceBack/src/main/java/com/example/SpaceBack/SpaceBackApplication.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/SpaceBackApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpaceBackApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpaceBackApplication.class, args);
 	}
+
 }
