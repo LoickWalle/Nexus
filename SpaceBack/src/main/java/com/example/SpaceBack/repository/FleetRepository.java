8f102f4544666b68053213fc9bbd983e53fa2568
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-17 16:15:49 +0200

    0015/fix: Add annotation

--- SpaceBack/src/main/java/com/example/SpaceBack/repository/FleetRepository.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/repository/FleetRepository.java
@@ -2,8 +2,10 @@ package com.example.SpaceBack.repository;
 
 import com.example.SpaceBack.model.Fleet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface FleetRepository extends JpaRepository<Fleet, UUID> {
 }
