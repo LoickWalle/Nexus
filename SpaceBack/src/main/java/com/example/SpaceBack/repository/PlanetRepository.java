04598f2fb2f50070af9051e4e9ae140c139f5cd5
LoickWalle <loickwalle@gmail.com>
2024-10-17 16:16:06 +0200

    0014/fix: add @Repository

--- SpaceBack/src/main/java/com/example/SpaceBack/repository/PlanetRepository.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/repository/PlanetRepository.java
@@ -2,9 +2,11 @@ package com.example.SpaceBack.repository;
 
 import com.example.SpaceBack.model.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PlanetRepository extends JpaRepository<Planet, UUID> {
     Planet findByName(String name);
 }
