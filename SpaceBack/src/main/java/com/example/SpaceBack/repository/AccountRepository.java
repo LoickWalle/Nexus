a54ff1fa13cb4bf74ca3f46ecabc1a3f69488ba8
LoickWalle <loickwalle@gmail.com>
2024-10-17 16:25:18 +0200

    0013/fix: add @Repository

--- SpaceBack/src/main/java/com/example/SpaceBack/repository/AccountRepository.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/repository/AccountRepository.java
@@ -2,9 +2,11 @@ package com.example.SpaceBack.repository;
 
 import com.example.SpaceBack.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
     Account findByEmail(String email);
 }
