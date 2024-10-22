6833f6ea0c82fb78462ab2cdec92dd1d3f7de3e0
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-22 15:36:43 +0200

    feat/0025: create record

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/ShipDto.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/ShipDto.java
@@ -1,16 +1,12 @@
 package com.example.SpaceBack.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-public class ShipDto {
-    String name;
-    int hitpoint;
-    int attackPower;
-    long speed;
-    int costMetal;
-    int costCristal;
-    int costDeuterium;
-}
+public record ShipDto (
+    String name,
+    int hitpoint,
+    int attackPower,
+    long speed,
+    int costMetal,
+    int costCristal,
+    int costDeuterium
+){}
