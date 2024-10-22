52447824c0c0f1dd3e0c74571a5b9a92eed29311
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-22 15:27:13 +0200

    0025/feat: add ShipDto

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/ShipDto.java
@@ -0,0 +1,16 @@
+package com.example.SpaceBack.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class ShipDto {
+    String name;
+    int hitpoint;
+    int attackPower;
+    long speed;
+    int costMetal;
+    int costCristal;
+    int costDeuterium;
+}
