ee23bc63e6374356e58c7de32bf0c18b91f98d8f
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-22 15:55:05 +0200

    0023/feat: add validation

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetDto.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetDto.java
@@ -1,17 +1,21 @@
 package com.example.SpaceBack.dto;
 
 import com.example.SpaceBack.model.FleetShip;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 
 public record FleetDto (
+        @NotBlank
         String name,
         int destinationX,
         int destinationY,
+        @DateTimeFormat
         LocalDate arrivalDate,
         Set<FleetShipDto> fleetShipsDto
 )
