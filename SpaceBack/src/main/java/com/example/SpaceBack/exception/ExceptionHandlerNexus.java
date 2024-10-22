65597a7673cf7c234fe0cfd0bff6343d8b2ba700
Loick Walle <loickwalle@gmail.com>
2024-10-22 15:23:56 +0200

    0042/feat: add BadRequestException (for @Valid)

--- SpaceBack/src/main/java/com/example/SpaceBack/exception/ExceptionHandlerNexus.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/exception/ExceptionHandlerNexus.java
@@ -1,8 +1,21 @@
 package com.example.SpaceBack.exception;
 
+import com.example.SpaceBack.dto.ExceptionDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @ControllerAdvice
 public class ExceptionHandlerNexus {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDTO> handleConflictException(MethodArgumentNotValidException ex){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(ex.getMessage(), ex.getStatusCode().value(), LocalDateTime.now());
+
+        return new ResponseEntity<>(exceptionDTO, ex.getStatusCode());
+    }
 }
