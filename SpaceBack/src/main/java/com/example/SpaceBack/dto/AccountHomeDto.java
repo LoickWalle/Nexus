89c84bc0237469f6810d8adeb814c0748cd4d16f
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-10-22 15:51:27 +0200

    0022/feat: create AccountHomeDto record

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/AccountHomeDto.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/AccountHomeDto.java
@@ -1,7 +1,13 @@
 package com.example.SpaceBack.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record AccountHomeDto(
+        @NotBlank
+        @Size(min = 4, max = 20)
         String username,
+
         String avatarUrl
 //        Set<PlanetDto> ownedPlanets
 ) {
