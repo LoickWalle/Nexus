2d0c3ed1f4c041c01bb251697c3799feeafe4d6f
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-23 14:48:38 +0200

    0030/feat: remove FleetDto in FleetShipDto

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetShipDto.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetShipDto.java
@@ -1,9 +1,9 @@
 package com.example.SpaceBack.dto;
 
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record FleetShipDto(
-        FleetDto fleetDto,
         ShipDto shipDto,
         @PositiveOrZero
         int quantity
