53ff807278eabde7af3c42695eec83ea032cb018
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-25 16:35:56 +0200

    0029/feat: Add ProductionBuildingDto

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/ProductionBuildingDto.java
@@ -0,0 +1,29 @@
+package com.example.SpaceBack.dto;
+
+import com.example.SpaceBack.utils.enums.ResourceType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ProductionBuildingDto(
+        @NotBlank
+        String name,
+        @NotBlank
+        String description,
+        @Positive
+        int level,
+        @PositiveOrZero
+        long costMetal,
+        @PositiveOrZero
+        long costCristal,
+        @PositiveOrZero
+        long costDeuterium,
+        @PositiveOrZero
+        long energyConsumption,
+        @PositiveOrZero
+        long productionRate,
+        @NotNull
+        ResourceType resourceType
+) {
+}
