9babd6e41af4ddfc436f52a5e8473a9a1b9ede86
Julien Brancourt <115453167+JulienBrancourt@users.noreply.github.com>
2024-10-25 16:51:57 +0200

    Update FleetComponentDto.java

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetComponentDto.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetComponentDto.java
@@ -1,10 +1,12 @@
 package com.example.SpaceBack.dto;
 
-
+import jakarta.validation.constraints.NotNull;
 import java.util.Set;
 
 public record FleetComponentDto(
+        @NotNull
         Set<FleetDto> FleetsDto,
+        @NotNull
         Set<ConstructionQueueDto> ConstructionQueuesDto
 ) {
 }
