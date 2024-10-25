1eca64d58354727d15a75143d7facf26d9e656c8
LoickWalle <loickwalle@gmail.com>
2024-10-25 15:57:07 +0200

    Merge Feature/0050 (#46)

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/RessourceDetailDTO.java
@@ -0,0 +1,22 @@
+package com.example.SpaceBack.dto;
+
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record RessourceDetailDTO (
+    @PositiveOrZero
+    long maxMetalCapacity,
+    @PositiveOrZero
+    long metalProductionRatePerHour,
+    @PositiveOrZero
+    long maxCristalCapacity,
+    @PositiveOrZero
+    long cristalProductionRatePerHour,
+    @PositiveOrZero
+    long maxDeuteriumCapacity,
+    @PositiveOrZero
+    long deuteriumProductionRatePerHour,
+    @PositiveOrZero
+    long energyProduction,
+    @PositiveOrZero
+    long energyConsumption
+){}
