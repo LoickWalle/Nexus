5ecfdaaf48852a486dcbde07d5b5f9580e5d5449
Loick Walle <loickwalle@gmail.com>
2024-10-25 16:22:49 +0200

    0032/feat: add LoginDto

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/LoginDto.java
@@ -0,0 +1,12 @@
+package com.example.SpaceBack.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginDto (
+    @Email
+    @NotBlank
+    String email,
+    @NotBlank
+    String password
+){}
