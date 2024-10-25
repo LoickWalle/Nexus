0897fd8b71294248c2c04cce2ec6e18d1af940af
Cédric B. <42787563+N-H-U-A-C@users.noreply.github.com>
2024-10-25 15:50:07 +0200

    Merge Feature/0027 (#42)

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/ConstructionQueueDto.java
@@ -0,0 +1,20 @@
+package com.example.SpaceBack.dto;
+
+import jakarta.validation.constraints.Positive;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+public record ConstructionQueueDto(
+        String buildingUrl,
+
+        @Positive
+        int buildingLevel,
+
+        @DateTimeFormat
+        LocalDateTime constructionEndTime,
+
+        @DateTimeFormat
+        LocalDateTime createdAt
+) {
+}
