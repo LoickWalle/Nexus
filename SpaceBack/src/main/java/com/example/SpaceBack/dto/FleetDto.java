d07cac47e8dfe8df175bf595bcb11038a2275f08
Julien Brancourt <115453167+JulienBrancourt@users.noreply.github.com>
2024-10-25 15:57:47 +0200

    Merge Feature/0045 (#47)

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetDto.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/FleetDto.java
@@ -1,13 +1,10 @@
 package com.example.SpaceBack.dto;
 
-import com.example.SpaceBack.model.FleetShip;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 
@@ -19,7 +16,7 @@ public record FleetDto (
         int destinationX,
         int destinationY,
         @DateTimeFormat
-        LocalDate arrivalDate,
-        Set<FleetShipDto> fleetShipsDto
+        LocalDateTime arrivalDate,
+        Set<ShipDto> ShipsDto
 )
 {}
