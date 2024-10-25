3cd650bcf79b79c48cbdd8fe06bdc2df7b856e27
Loick Walle <loickwalle@gmail.com>
2024-10-25 16:34:16 +0200

    0055/feat: add AccountModifiedDto

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/AccountModifiedDto.java
@@ -0,0 +1,19 @@
+package com.example.SpaceBack.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record AccountModifiedDto (
+    String newUsername,
+    @Email
+    @NotBlank
+    String email,
+    String newPassword,
+    String previousPassword,
+    String newAvatarUrl,
+    @NotNull
+    LocalDateTime createdAt
+){}
