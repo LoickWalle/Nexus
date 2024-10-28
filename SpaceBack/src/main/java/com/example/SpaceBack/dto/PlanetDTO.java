81416700f673536ec93437cde65b0f946231ee05
LoickWalle <loickwalle@gmail.com>
2024-10-28 14:33:16 +0100

    Merge Feature/0024 - add PlanetDTO (#61)

---
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/PlanetDTO.java
@@ -0,0 +1,16 @@
+package com.example.SpaceBack.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
+
+public record PlanetDTO (
+    @NotBlank
+    @Size(max = 20)
+    String name,
+    long coordinateX,
+    long coordinateY,
+    @NotNull
+    Set<ConstructionQueueDto> queueDTOs
+){}
