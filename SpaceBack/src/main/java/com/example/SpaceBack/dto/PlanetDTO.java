4bc02c9f70d4ad4b1dbccef7732928acd98fe721
Loick Walle <loickwalle@gmail.com>
2024-10-29 15:28:25 +0100

    0035/feature: add PlanetService

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/PlanetDTO.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/PlanetDTO.java
@@ -2,6 +2,7 @@ package com.example.SpaceBack.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
---
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/PlanetService.java
@@ -0,0 +1,15 @@
+package com.example.SpaceBack.service;
+
+import com.example.SpaceBack.dto.PlanetDTO;
+import com.example.SpaceBack.dto.ResourceCurrentDTO;
+import com.example.SpaceBack.dto.ResourceDTO;
+import com.example.SpaceBack.dto.ResourceDetailDTO;
+
+import java.util.UUID;
+
+public interface PlanetService {
+    PlanetDTO getPlanetById(UUID planetId);
+    ResourceCurrentDTO getResourceCurrent(UUID planetId);
+    ResourceDetailDTO getResourceDetail(UUID planetId);
+    ResourceDTO getResource(UUID planetId);
+}
