126a89d3207f647e56a27970ad10d45c9ca7a89c
ElyanisJ <brancourt.julien@hotmail.fr>
2024-10-29 10:31:58 +0100

    0036/feat: update ShipService interface

--- SpaceBack/src/main/java/com/example/SpaceBack/service/ShipService.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/ShipService.java
@@ -6,6 +6,6 @@ import com.example.SpaceBack.model.Ship;
 import java.util.Set;
 
 public interface ShipService {
-    ShipDTO getByName(ShipDTO shipToFound);
-    Set<ShipDTO> getAllShipDTO();
+    ShipDTO getShipByName(String name);
+    Set<ShipDTO> getAllShips();
 }
