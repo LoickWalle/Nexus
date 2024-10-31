0244f03a49783fa96f683bb163ed7040b7e44f9b
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-10-31 15:47:35 +0100

    0062/test: write unit tests for AccountMapper

--- SpaceBack/src/test/java/com/example/SpaceBack/service/mapper/AccountMapperTest.java
+++ SpaceBack/src/test/java/com/example/SpaceBack/service/mapper/AccountMapperTest.java
@@ -3,30 +3,30 @@ package com.example.SpaceBack.service.mapper;
 import com.example.SpaceBack.dto.AccountDTO;
 import com.example.SpaceBack.dto.AccountShortDTO;
 import com.example.SpaceBack.model.Account;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AccountMapperTest {
 
+    LocalDateTime createdAt;
     Account account;
 
     @BeforeEach
     public void setUp() {
+        createdAt = LocalDateTime.now();
         account = Account.builder()
                 .id(UUID.randomUUID())
                 .username("toto")
-                .email("toto@email.com")
+                .email("toto@gmail.com")
                 .password("password")
-                .createdAt(LocalDate.now())
+                .createdAt(createdAt)
                 .avatarUrl("url")
                 .build();
     }
@@ -40,12 +40,18 @@ class AccountMapperTest {
         AccountShortDTO result = AccountMapper.INSTANCE_ACCOUNT.accountToAccountShortDTO(account);
 
         // then
-        assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void account_Should_Map_To_AccountDTO() {
         // given
-        AccountDTO expected = new AccountDTO("tata", "toto@gmail.com", "url", LocalDateTime.now());
+        AccountDTO expected = new AccountDTO("toto", "toto@gmail.com", "url", createdAt);
+
+        // when
+        AccountDTO result = AccountMapper.INSTANCE_ACCOUNT.accountToAccountDTO(account);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
