b702a87e0c66dec1fc445dd09d7fa3cc24593098
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-10-31 18:41:54 +0100

    0073/feat: add shipAvatarURL field to ShipDTO record

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/ShipDTO.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/ShipDTO.java
@@ -5,20 +5,23 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record ShipDTO(
-    @NotBlank
-    String name,
-    @PositiveOrZero
-    int hitpoint,
-    @PositiveOrZero
-    int attackPower,
-    @PositiveOrZero
-    long speed,
-    @PositiveOrZero
-    long costMetal,
-    @PositiveOrZero
-    long costCristal,
-    @PositiveOrZero
-    long costDeuterium,
-    @PositiveOrZero
-    int quantity
-){}
+        @NotBlank
+        String name,
+        @PositiveOrZero
+        int hitpoint,
+        @PositiveOrZero
+        int attackPower,
+        @PositiveOrZero
+        long speed,
+        @PositiveOrZero
+        long costMetal,
+        @PositiveOrZero
+        long costCristal,
+        @PositiveOrZero
+        long costDeuterium,
+        @NotBlank
+        String shipAvatarURL,
+        @PositiveOrZero
+        int quantity
+) {
+}
