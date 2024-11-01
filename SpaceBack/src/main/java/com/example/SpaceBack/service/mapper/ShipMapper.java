6fd2a6faa26271521816816b430b0583af93d5e7
ElyanisJ <brancourt.julien@hotmail.fr>
2024-11-01 16:45:00 +0100

    0065/refactor: remove Mapping quantity

--- SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/ShipMapper.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/ShipMapper.java
@@ -16,7 +16,6 @@ public interface ShipMapper {
     ShipDataDTO shipToShipDataDTO(Ship ship);
 
     //ShipPlayerDTO
-    @Mapping(source = "fleetShip.quantity", target = "quantity")
     @Mapping(source = "fleetShip.ship.name", target = "name")
     @Mapping(source = "fleetShip.ship.hitpoint", target = "hitpoint")
     @Mapping(source = "fleetShip.ship.attackPower", target = "attackPower")
@@ -25,6 +24,5 @@ public interface ShipMapper {
     @Mapping(source = "fleetShip.ship.costCristal", target = "costCristal")
     @Mapping(source = "fleetShip.ship.costDeuterium", target = "costDeuterium")
     @Mapping(source = "fleetShip.ship.shipAvatarURL", target = "shipAvatarURL")
-    @Mapping(source = "fleetShip.ship.id", target = "id")
-    ShipPlayerDTO shipToShipPlayerDTO(FleetShip fleetShip);
+    ShipPlayerDTO fleetShipToShipPlayerDTO(FleetShip fleetShip);
 }
