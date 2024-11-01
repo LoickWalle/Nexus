9359c511851ae8960afaa363bdcaad07840221b0
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-11-01 15:15:35 +0100

    0077/style: rename DefenseDTO to DefensePlayerDTO

--- SpaceBack/src/main/java/com/example/SpaceBack/dto/DefensePlayerDTO.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/dto/DefensePlayerDTO.java
@@ -3,7 +3,7 @@ package com.example.SpaceBack.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 
-public record DefenseDTO(
+public record DefensePlayerDTO(
         @NotBlank
         String name,
 
