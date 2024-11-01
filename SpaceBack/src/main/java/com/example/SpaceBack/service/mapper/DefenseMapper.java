9e9f5e0e6ebff78b517a58990cdea59006064a73
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-11-01 16:02:47 +0100

    0065/feat: add defenseToDefenseDataDTO method to DefenseMapper interface

--- SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/DefenseMapper.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/DefenseMapper.java
@@ -1,5 +1,7 @@
 package com.example.SpaceBack.service.mapper;
 
+import com.example.SpaceBack.dto.DefenseDataDTO;
+import com.example.SpaceBack.model.Defense;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -7,4 +9,7 @@ import org.mapstruct.factory.Mappers;
 public interface DefenseMapper {
 
     DefenseMapper INSTANCE_DEFENSE = Mappers.getMapper(DefenseMapper.class);
+
+    // DefenseDataDTO
+    DefenseDataDTO defenseToDefenseDataDTO(Defense defense);
 }
