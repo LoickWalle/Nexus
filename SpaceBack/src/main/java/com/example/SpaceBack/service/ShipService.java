2e09f9787ad3611ccab87dfc31532b1f3562a19c
ElyanisJ <brancourt.julien@hotmail.fr>
2024-11-01 14:57:34 +0100

    0076/feat: rename Ship DTO in ShipService

--- SpaceBack/src/main/java/com/example/SpaceBack/service/ShipService.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/ShipService.java
@@ -1,10 +1,11 @@
 package com.example.SpaceBack.service;
 
+import com.example.SpaceBack.dto.ShipDataDTO;
 import com.example.SpaceBack.dto.ShipPlayerDTO;
 
 import java.util.Set;
 
 public interface ShipService {
-    ShipPlayerDTO getShipByName(String name);
-    Set<ShipPlayerDTO> getAllShips();
+    ShipDataDTO getShipByName(String name);
+    Set<ShipDataDTO> getAllShips();
 }
