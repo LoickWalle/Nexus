b3143647cec3032e02702933a77dbab0a9b24ff9
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-11-12 15:08:24 +0100

    0078/feat: update login component

--- SpaceFront/src/app/components/login/login.component.ts
+++ SpaceFront/src/app/components/login/login.component.ts
@@ -1,18 +1,22 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Login} from '../../models/login.interface';
+import {ButtonComponent} from '../button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ButtonComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  BUTTON_LABEL: string = "Se connecter";
 
   formLogin = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -23,6 +27,7 @@ export class LoginComponent {
   log() {
     if (this.formLogin.valid) {
       console.log(this.formLogin.value as Login);
+      this.formLogin.reset();
     }
   }
 
