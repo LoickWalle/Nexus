d06a227452b4d888554038c06bc3187d6f53fd87
N-H-U-A-C <cedric.belot.dev@gmail.com>
2024-11-20 16:33:56 +0100

    0066/feat: add queueItemWithBuildingToQueueItemDTO method to QueueItemMapper

--- SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/QueueItemMapper.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/QueueItemMapper.java
@@ -1,10 +1,18 @@
 package com.example.SpaceBack.service.mapper;
 
+import com.example.SpaceBack.dto.QueueItemDTO;
+import com.example.SpaceBack.model.QueueItem;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface QueueItemMapper {
 
     QueueItemMapper INSTANCE_QUEUE_ITEM = Mappers.getMapper(QueueItemMapper.class);
+
+    // QueueItemDTO
+    @Mapping(target = "constructionURL", source = "queueItem.constructionURL")
+    @Mapping(target = "createdAt", source = "queueItem.createdAt")
+    QueueItemDTO queueItemWithBuildingToQueueItemDTO(QueueItem queueItem, String name, String constructionUrl, int buildingLevel);
 }
