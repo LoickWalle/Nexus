dc3fd15bad0acec5c43c42eada3725554eed4f86
N-H-U-A-C <cedric.belot.dev@gmail.com>
2024-11-25 15:58:42 +0100

    0063/perf: clean imports for PlanetMapper interface

--- SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/PlanetMapper.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/service/mapper/PlanetMapper.java
@@ -4,7 +4,6 @@ import com.example.SpaceBack.dto.PlanetDTO;
 import com.example.SpaceBack.dto.QueueItemDTO;
 import com.example.SpaceBack.model.Planet;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.Set;
