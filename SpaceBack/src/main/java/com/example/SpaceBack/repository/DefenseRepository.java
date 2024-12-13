3b9d0a602a7e44d2396a0832d143fcdf18a68f51
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-12-13 15:42:30 +0100

    0087/feat: add findAllPlanetDefensesByPlanetCoordinates method to DefenseRepository interface

--- SpaceBack/src/main/java/com/example/SpaceBack/repository/DefenseRepository.java
+++ SpaceBack/src/main/java/com/example/SpaceBack/repository/DefenseRepository.java
@@ -1,11 +1,24 @@
 package com.example.SpaceBack.repository;
 
 import com.example.SpaceBack.model.Defense;
+import com.example.SpaceBack.model.PlanetDefense;
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface DefenseRepository extends JpaRepository<Defense, UUID> {
+    // 1) sous-requête id planet
+    // 2) requête defense by planet (planetdefense) id
+    @Transactional
+    @Query(value = "select pd from PlanetDefense pd where pd.planet.coordinateX = :coordinateX and pd.planet.coordinateY = :coordinateY")
+    Set<PlanetDefense> findAllPlanetDefensesByPlanetCoordinates(int coordinateX, int coordinateY);
+
+    Optional<Defense> findDefenseByName(@NotBlank String name);
 }
